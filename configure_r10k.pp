750fb6bb8151aebc8d25e7da50660a70ac8ca4ef
Gary Larizza <gary@puppetlabs.com>
2014-08-27 15:06:52 -0700

    Add a Puppet manifest to configure R10k

---
+++ configure_r10k.pp
@@ -0,0 +1,20 @@
+######         ######
+##  Configure R10k ##
+######         ######
+
+##  This manifest requires the zack/R10k module and will attempt to
+##  configure R10k according to my blog post on directory environments.
+##  Beware! (and good luck!)
+
+class { 'r10k':
+  version           => '1.3.2',
+  sources           => {
+    'puppet' => {
+      'remote'  => 'https://github.com/glarizza/puppet_repository.git',
+      'basedir' => "${::settings::confdir}/environments",
+      'prefix'  => false,
+    }
+  },
+  purgedirs         => ["${::settings::confdir}/environments"],
+  manage_modulepath => false,
+}
