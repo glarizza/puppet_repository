b5f8bbb59ffb398000668a5a66f565bd966f2e59
jpicklyk <jeff_picklyk@hotmail.com>
2014-10-03 10:45:30 -0400

    Update configure_r10k.pp
    Changing the repo to be my own.

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -10,7 +10,7 @@ class { 'r10k':
   version           => '1.3.2',
   sources           => {
     'puppet' => {
-      'remote'  => 'https://github.com/glarizza/puppet_repository.git',
+      'remote'  => 'https://github.com/jpicklyk/puppet_repository.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
     }
