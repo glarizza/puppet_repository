bbaf5489afd8b59e9c3f1fa287664b3ed8892449
Gary Larizza <gary@puppetlabs.com>
2016-01-21 18:18:58 -0800

    New config_version for 2015.x and Code Manager

---
+++ scripts/config_version.rb
@@ -0,0 +1,23 @@
+begin
+  require 'rugged'
+rescue LoadError => e
+  t = Time.new
+  puts t.to_i
+else
+
+  environmentpath = ARGV[0]
+  environment     = ARGV[1]
+
+  repo = Rugged::Repository.discover(File.join(environmentpath, environment))
+  head  = repo.head
+
+  #sha1 hash of the newest commit
+  head_sha = head.target_id
+
+  #the commit message associated the newest commit
+  commit = repo.lookup(head_sha)
+
+  #add something to find the remote url
+
+  puts head_sha 
+end
